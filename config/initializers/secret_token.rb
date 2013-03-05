1280a4d410625e24b045590dbc1b8fb7cfc89e7d
LD <ldressel@gmail.com>
2013-03-05 13:06:05 -0800

    end of chapter 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = '11eaedd3a40fb60f7fe0e43e0a34557c6ba861eb0c4d90103b2ce4124fd1aa93359c04029412d22e7aebe1dbd69273e2df7f024b2e562313ccdec5ec0644671a'
